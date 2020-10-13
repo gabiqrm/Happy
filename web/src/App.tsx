cd763016388d18902e824d8022bd526ddcd92a71
Gabriela.Queiroz <gabrielaqueirozrm@gmail.com>
2020-10-13 01:57:10 -0300

    :tada: First day of NLW3 : Creating Front-End

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './styles/global.css';
+
+import Routes from './routes';
+import { Router } from 'react-router-dom';
+
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
